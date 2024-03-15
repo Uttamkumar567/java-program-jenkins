419873e15b2d63f2d974dda8c17a942db906950b
Uttamkumar567 <43989610+Uttamkumar567@users.noreply.github.com>
2024-03-15 12:50:48 +0530

    Update simple.java
    deleted one file

--- simple.java
+++ simple.java
@@ -2,7 +2,7 @@ public class simple {
     public static void main(String[] args) {
         System.out.println("Tis is java program");
         System.out.println("Output from java program");
-        System.out.println("Added another file");
+        
         
     }
     
