793c0438fc148541683a73e357b4be5baf59a1f2
Uttamkumar567 <uttamsharma9852@gmail.com>
2024-03-15 12:13:14 +0530

    Addad simple.java file

--- ds.java
+++ ds.java
@@ -1,10 +1,10 @@
-import java.util.date;
+import java.util.Date;
 public class ds {
 
     public static void main(String[] args) {
         System.out.println("This is java program");
         System.out.println("Output from java program");
-        System.out.println(new date());
+        System.out.println(new Date());
         System.out.println("Hello");
     }
 }
---
+++ simple.java
@@ -0,0 +1,8 @@
+public class simple {
+    public static void main(String[] args) {
+        System.out.println("Tis is java program");
+        System.out.println("Output from java program");
+        
+    }
+    
+}
