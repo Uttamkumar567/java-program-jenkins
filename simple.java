5f9a6b5523f4453989b9dfffc76457bbe3b7d051
Uttamkumar567 <uttamsharma9852@gmail.com>
2024-03-15 13:19:24 +0530

    Added student file

--- simple.java
+++ simple.java
@@ -2,7 +2,8 @@ public class simple {
     public static void main(String[] args) {
         System.out.println("Tis is java program");
         System.out.println("Output from java program");
-        
+        Student student = new Student();
+        student.sayHello();
         
     }
     
